daafa5bc9af105d9cb6804c78128e824fd498105
Austen Ito <austen.ito@gmail.com>
2013-12-10 20:26:54 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tortue::Application.config.secret_key_base = '3111527f99cf115386b0091f0de8fc074043bf202077cebb74f8a71a1a72541d6546950ceb0a4043be705e8b403e92834faac3e0e7c2a8ec4eede7482611f001'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tortue::Application.config.session_store :cookie_store, key: '_tortue_session'
